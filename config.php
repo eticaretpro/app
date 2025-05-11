edb6ef3331e442a207d30f11f7c587983663d3e5
eticaret.pro <132004562+eticaretpro@users.noreply.github.com>
2025-05-12 00:00:36 +0300

    Delete config.php

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-/* Configuration of the site */
-define('DATABASE_SERVER',   'localhost');
-define('DATABASE_USERNAME', 'app_users');
-define('DATABASE_PASSWORD', 'Tayfun523641');
-define('DATABASE_NAME',     'app_databs');
-define('SITE_URL',          'https://app.eticaret.pro/');
